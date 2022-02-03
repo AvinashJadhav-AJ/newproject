e1fd4f5caaa1410750296fc66c0385a96275fd93
Avinash <aj912873@gmail.com>
2022-02-03 08:47:36 +0530

    Subtraction

---
+++ eclipse-workspace/HelloWorld/src/com/HelloWorld/Subtraction.java
@@ -0,0 +1,13 @@
+package com.HelloWorld;
+
+public class Subtraction {
+	public static void main(String[] args)
+	{ 
+		int a = 1;
+		int b = 2;
+		int subtraction = a - b;
+		System.out.println(subtraction+"subtraction");	
+	}
+}
+
+
