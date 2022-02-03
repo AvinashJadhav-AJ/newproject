ba7c027c82811320ce2fe06f8412f0e81de76476
Avinash <aj912873@gmail.com>
2022-02-03 08:53:11 +0530

    Added: Subtraction of two variable

--- eclipse-workspace/HelloWorld/src/com/HelloWorld/Subtraction.java
+++ eclipse-workspace/HelloWorld/src/com/HelloWorld/Subtraction.java
@@ -6,7 +6,7 @@ public class Subtraction {
 		int a = 1;
 		int b = 2;
 		int subtraction = a - b;
-		System.out.println(subtraction+"subtraction");	
+		System.out.println(subtraction+"subtraction is");	
 	}
 }
 
