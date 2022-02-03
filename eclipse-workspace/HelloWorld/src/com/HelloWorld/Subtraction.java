c0f8dee6072187a3d05a41ae68e5929664905d82
Avinash <aj912873@gmail.com>
2022-02-03 08:56:29 +0530

    Modified: Variable name change

--- eclipse-workspace/HelloWorld/src/com/HelloWorld/Subtraction.java
+++ eclipse-workspace/HelloWorld/src/com/HelloWorld/Subtraction.java
@@ -3,10 +3,10 @@ package com.HelloWorld;
 public class Subtraction {
 	public static void main(String[] args)
 	{ 
-		int a = 1;
-		int b = 2;
-		int subtraction = a - b;
-		System.out.println(subtraction+"subtraction is");	
+		int num1 = 1;
+		int num2 = 2;
+		int subtraction = num1 - num2;
+		System.out.println("subtraction is"+ subtraction);	
 	}
 }
 
